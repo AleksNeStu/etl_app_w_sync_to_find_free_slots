b26f9fef13e2cbe7015d0ef066c457ca87062042
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-08 18:42:58 +0100

    Modify env.py for alembic setup with SA and project

--- migrations/alembic/env.py
+++ migrations/alembic/env.py
@@ -1,9 +1,10 @@
+import os
+import sys
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,17 +14,30 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
 
+# METADATA
+# add_module_to_sys_path
+directory = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../meet_app'))
+sys.path.insert(0, directory)
+# noinspection PyUnresolvedReferences
+import settings
+from data.models.modelbase import SqlAlchemyBase
+target_metadata = SqlAlchemyBase.metadata
+
+
+# OPTIONS alembic.ini
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+# https://alembic.sqlalchemy.org/en/latest/api/config.html
+config.set_main_option("sqlalchemy.url", settings.DB_CONNECTION)
 
+
+# MOD
+# migration_context = context.get_context()
+# migration_current_rev = migration_context.get_current_revision()
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
