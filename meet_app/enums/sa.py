4548358e6587448422c4e386b8a69c02d9513009
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-08 21:46:20 +0100

    Make enums used for sa ORM serializable

--- meet_app/enums/sa.py
+++ meet_app/enums/sa.py
@@ -1,8 +1,18 @@
 import enum
 
+# To avoid {TypeError}Object of type xxx is not JSON serializable use mixin str
+# https://stackoverflow.com/questions/24481852/serialising-an-enum-member-to-json
+@enum.unique
+class SyncStatus(str, enum.Enum):
+    started: str = 'started'
+    skipped: str = 'skipped'
+    got_data: str = 'got_data'
+    finished: str = 'finished' # meaning got data was parsed
+    errors: str = 'errors'
 
-class SyncStatus(enum.Enum):
-    in_progress = 'in_progress'
-    finished = 'finished'
-    skipped = 'skipped'
-    errors = 'errors'
+@enum.unique
+class SyncEndReason(str, enum.Enum):
+    no_new_remote_data: str = 'no_new_remote_data'
+    remote_server_errors: str = 'remote_server_errors'
+    data_parsing_errors: str = 'data_parsing_errors'
+    data_parsing_end: str = 'data_parsing_end'
