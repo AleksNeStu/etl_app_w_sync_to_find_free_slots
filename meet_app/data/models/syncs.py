8653280f3ac1b47c7fc31c9043f8533604ba9844
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-20 01:57:17 +0100

    Fix Syncs integer types and add relationship to Users.

--- meet_app/data/models/syncs.py
+++ meet_app/data/models/syncs.py
@@ -13,9 +13,9 @@ from enums.sa import SyncStatus, SyncEndReason, NotSyncedItemReason
 @dataclass
 class NotSyncedItem(SqlAlchemyBase):
     __tablename__ = 'not_synced_items'
-    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     sync_id: int = sa.Column(
-        sa.BigInteger, sa.ForeignKey('syncs.id'), nullable=False, index=True)
+        sa.Integer, sa.ForeignKey('syncs.id'), nullable=False, index=True)
     item_data: Text = sa.Column(sa.String, nullable=False)
     reason: str = sa.Column(sa.Enum(NotSyncedItemReason))
 
@@ -26,7 +26,7 @@ class NotSyncedItem(SqlAlchemyBase):
 class Sync(SqlAlchemyBase):
     __tablename__ = 'syncs'
 
-    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     start_date: datetime.datetime = sa.Column(
         sa.DateTime, index=True, nullable=False)
     end_date: datetime.datetime = sa.Column(
@@ -39,15 +39,15 @@ class Sync(SqlAlchemyBase):
 
 
     #TODO: sqlalchemy.orm.exc.DetachedInstanceError: Parent instance <Sync at 0x7fc1d3099c00> is not bound to a Session; lazy load operation of attribute 'users' cannot proceed (Background on this error at: https://sqlalche.me/e/14/bhk3)
-    #
-    # # extracted users per sync
-    # from data.models.users import User
-    # # Not `backref` to make attr for User explicit
-    # users: List[User] = orm.relationship(
-    #     User.__name__, back_populates='sync', order_by=[
-    #         User.name.desc(), User.hash_id.desc(),
-    #     ])
-    #
+
+    # extracted users per sync
+    from data.models.users import User
+    # Not `backref` to make attr for User explicit
+    users: List[User] = orm.relationship(
+        User.__name__, back_populates='sync', order_by=[
+            User.name.desc(), User.hash_id.desc(),
+        ])
+
     # # # extracted meets per sync
     # # from data.models.meets import Meet
     # # meets = orm.relationship(Meet.__name__, backref='sync')
