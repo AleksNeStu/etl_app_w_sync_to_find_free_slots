da1f1fcfa3223d950ab4f580d086d330f38a69a5
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-20 01:54:50 +0100

    Add UniqueMixin for ORM classes to check and avoid errors when duplication unique data.

--- meet_app/data/models/modelbase.py
+++ meet_app/data/models/modelbase.py
@@ -7,3 +7,49 @@ import sqlalchemy.ext.declarative as dec
 
 
 SqlAlchemyBase = dec.declarative_base()
+
+
+def _unique(session, cls, hashfunc, queryfunc, constructor, arg, kw):
+    """Provide the "guts" to the unique recipe.
+
+    Is given a Session to work with, and associates a dictionary with
+    the Session() which keeps track of current "unique" keys.
+    """
+    cache = getattr(session, '_unique_cache', None)
+    if cache is None:
+        session._unique_cache = cache = {}
+
+    key = (cls, hashfunc(*arg, **kw))
+    if key in cache:
+        return cache[key]
+    else:
+        with session.no_autoflush:
+            q = session.query(cls)
+            q = queryfunc(q, *arg, **kw)
+            obj = q.first()
+            if not obj:
+                obj = constructor(*arg, **kw)
+                session.add(obj)
+        cache[key] = obj
+        return obj
+
+
+class UniqueMixin(object):
+    @classmethod
+    def unique_hash(cls, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def unique_filter(cls, query, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def as_unique(cls, session, *arg, **kw):
+        return _unique(
+            session,
+            cls,
+            cls.unique_hash,
+            cls.unique_filter,
+            cls,
+            arg, kw
+        )
