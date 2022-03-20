968699c0e6aec6eb9da7175db028d81c423643b5
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-20 01:56:42 +0100

    Add uniqueness checks for Users table to skip duplication hash_id errors during forced or new syncs.

--- meet_app/data/models/users.py
+++ meet_app/data/models/users.py
@@ -4,28 +4,36 @@ from dataclasses import dataclass
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
-from data.models.modelbase import SqlAlchemyBase
+from data.models.modelbase import SqlAlchemyBase, UniqueMixin
 
 
 @dataclass
-class User(SqlAlchemyBase):
+class User(UniqueMixin, SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
 
     # Hash of unique user, calculated by 3rd party service
     hash_id: str = sa.Column(
         sa.String, nullable=False, unique=True, index=True)
 
     sync_id: int = sa.Column(
-        sa.BigInteger, sa.ForeignKey('syncs.id'), nullable=False, index=True)
+        sa.Integer, sa.ForeignKey('syncs.id'), nullable=False, index=True)
 
     # 1st amd 2nd user name, from 3rd party service (duplication are possible,
     # cause supposed 3rd party service generated it based on additional info)
     name: str = sa.Column(sa.String, nullable=False, index=True)
 
     created_date: datetime.datetime = sa.Column(
-        sa.DateTime, default=datetime.datetime.now)
+        sa.DateTime, default=datetime.datetime.utcnow)
     updated_date: datetime.datetime = sa.Column(sa.DateTime, nullable=True)
 
     sync = orm.relationship('Sync')
+
+    @classmethod
+    def unique_hash(cls, hash_id):
+        return hash_id
+
+    @classmethod
+    def unique_filter(cls, query, hash_id):
+        return query.filter(User.hash_id == hash_id)
