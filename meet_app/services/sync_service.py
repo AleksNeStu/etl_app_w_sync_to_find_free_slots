9a9b9dbcbaec9fff775cf7001ed550218d59dc64
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-03-20 17:09:50 +0100

    Refactor sync_service.py in scope of query vars

--- meet_app/services/sync_service.py
+++ meet_app/services/sync_service.py
@@ -9,7 +9,7 @@ from enums.sa import SyncStatus
 from utils import py as py_utils
 
 
-def get_syncs(session: orm.Session = None) -> orm.Query:
+def get_syncs_qr(session: orm.Session = None) -> orm.Query:
     if session:
         return session.query(Sync)
 
@@ -18,28 +18,28 @@ def get_syncs(session: orm.Session = None) -> orm.Query:
 
 
 def get_latest_finished_sync(session: orm.Session = None,
-                             syncs: orm.Query = None) -> Optional[Sync]:
-    syncs = syncs or get_syncs(session)
-    res = syncs.filter(Sync.status == SyncStatus.finished).order_by(
+                             syncs_qr: orm.Query = None) -> Optional[Sync]:
+    syncs_qr = syncs_qr or get_syncs_qr(session)
+    res = syncs_qr.filter(Sync.status == SyncStatus.finished).order_by(
         Sync.id.desc()).first()
     # syncs.session.close()
 
     return res
 
 def get_latest_sync(session: orm.Session = None,
-                    syncs: orm.Query = None) -> Optional[Sync]:
-    syncs = syncs or get_syncs(session)
-    res = syncs.order_by(Sync.id.desc()).first()
+                    syncs_qr: orm.Query = None) -> Optional[Sync]:
+    syncs_qr = syncs_qr or get_syncs_qr(session)
+    res = syncs_qr.order_by(Sync.id.desc()).first()
     # syncs.session.close()
 
     return res
 
 
 def get_sync(get_kwargs: dict, session: orm.Session = None,
-             syncs: orm.Query = None) -> Optional[Sync]:
-    syncs = syncs or get_syncs(session)
+             syncs_qr: orm.Query = None) -> Optional[Sync]:
+    syncs_qr = syncs_qr or get_syncs_qr(session)
     _and = [getattr(Sync, k) == v for k, v in get_kwargs.items()]
-    res = syncs.filter(and_(*_and)).order_by(Sync.id.desc()).first()
+    res = syncs_qr.filter(and_(*_and)).order_by(Sync.id.desc()).first()
 
     return res
 
